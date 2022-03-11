5a6d18ff634918b3adcd4f4a4852803df7e551ab
yehoslavrudenco@gmail.com <yehoslavrudenco@gmail.com>
2022-03-11 22:22:26 +0200

    Created a simple but working implementation of wps editing.

--- website/welding/svg_edit.py
+++ website/welding/svg_edit.py
@@ -1,5 +1,9 @@
 """Testing editing svgs from python"""
+import os
+
 from cairosvg import svg2pdf
+
+from website.settings import WELDING
 
 
 class WPSMessages:
@@ -10,8 +14,47 @@ class WPSMessages:
     material: str
     preparation: str
 
+    def __init__(self, messages: dict | None):
+        if messages is not None:
+            self.set_attributes(messages)
+        else:
+            self.set_attributes(
+                {
+                    'thickness': 'th',
+                    'bevel': '',
+                    'joint': '',
+                    'material': '',
+                    'preparation': ''
+                }
+            )
+
+    def set_attributes(self, attributes: dict):
+        self.thickness = attributes['thickness']
+        self.bevel = attributes['bevel']
+        self.joint = attributes['joint']
+        self.material = attributes['material']
+        self.preparation = attributes['preparation']
+
     def __str__(self):
         return f"thickness: {self.thickness}\nbevel: {self.bevel}"
+
+
+def generate_svg(data: WPSMessages):
+    """Generates a WPS with the given data."""
+    with open(os.path.join(WELDING, 'test.svg'), 'r', encoding='UTF-8') as file:
+        dwg = file.read()
+
+    dwg = dwg.replace('$manuf', 'Wojciech Grzegorczyk') \
+        .replace('$material', data.material) \
+        .replace('$joint_nr', 'P1') \
+        .replace('$joint_type', data.joint) \
+        .replace('$thickness', data.thickness) \
+        .replace('$position', 'flat PA') \
+        .replace('$out_diameter', '---') \
+        .replace('$clean', data.preparation) \
+        .replace('$sealing_run', 'Single-side welding')
+
+    return dwg
 
 
 def generate_pdf(data: WPSMessages):
--- website/wps_view.py
+++ website/wps_view.py
@@ -1,10 +1,26 @@
 """The view to crate a WPS."""
 from flask import Blueprint
+from flask import render_template, request
+from flask import Markup
 
 
 wps_view = Blueprint('wps_view', __name__)
 
+
 @wps_view.route('/')
-def home():
+def homepage():
+    return render_template("home.html")
+
+
+@wps_view.route('/wps', methods=['GET', 'POST'])
+def edit_wps():
     """The homepage."""
-    return "<h1>Create your WPS here</h1>"
+    if request.method == 'POST':
+        # TODO: Add to chash template.svg, the wps_data, and the generated svg
+        from website.welding.svg_edit import WPSMessages
+        from website.welding.svg_edit import generate_svg
+        message = WPSMessages(None)
+        message.thickness = request.form.get('thickness')
+        dwg = generate_svg(message)
+        return render_template("wps_input.html", dwg=Markup(dwg))
+    return render_template("wps_input.html", dwg="")
