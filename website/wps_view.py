98502d3bd1e35d7294753ffd96498bb5cd139070
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-04-13 19:23:52 +0300

    Experimented with some things (some of them I commented). Added a post conection to the server (that does not require a page refresh). Based on welding procedure the layout will change.

--- website/wps_view.py
+++ website/wps_view.py
@@ -1,6 +1,6 @@
 """The view to crate a WPS."""
 from flask import Blueprint
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from flask import Markup
 
 wps_view = Blueprint('wps_view', __name__)
@@ -16,35 +16,63 @@ def aboutpage():
     return render_template("about.html")
 
 
+@wps_view.route('/test', methods=['GET', 'POST'])
+def wpss():
+    if request.method == 'POST':
+        from website.welding.standards import ISO2560, SubType, ISOError
+        data = request.get_json()
+        try:
+            match data:
+                case {'electrode': el, 'norm': 'EN ISO 2560-A'}:
+                    val = ISO2560().decode(el, SubType.A)
+                case {'electrode': el, 'norm': 'EN ISO 2560-B'}:
+                    val = ISO2560().decode(el, SubType.B)
+                case _:
+                    val = data
+
+            print(val)
+            return val, 200
+        except Exception as err:
+            print(err)
+            return 'BAD', 200
+
+    elif request.method == 'GET':
+        return 'some text'
+
+
 @wps_view.route('/wps', methods=['GET', 'POST'])
 def edit_wps():
     """The homepage."""
     if request.method == 'POST':
         # TODO: Add to chash template.svg, the wps_data, and the generated svg
-        from website.welding.svg_edit import WPSMessages
+        from website.welding.svg_edit import WPSData
         from website.welding.svg_edit import generate_svg
         from website.welding.svg_edit import FillerMaterial
         from website.welding.svg_edit import BaseMaterial
-        message = WPSMessages(
+        form_data = request.form.to_dict()
+        i = 1 if 'similarMaterial' in form_data else 2
+        message = WPSData(
             filler_material=FillerMaterial(
-                name=request.form.get('filler-material'),
-                norm="norma",
-                thickness=0,
-                dry=True,
+                name=form_data['filler-material'],
+                norm=form_data['filler-norm'],
+                thickness=float(form_data['filler_thickness']),
+                dry='dry' in form_data,
+                procedure_type=form_data['procedee'],
             ),
             base_material1=BaseMaterial(
-                thickness=float(request.form.get('thickness1')),
-                group='grupa',
-                norm='norma',
-                name=request.form.get('steel-grade1')
+                thickness=float(form_data['thickness1']),
+                group=form_data['steel-group1'],
+                norm=form_data['steel-norm1'],
+                name=form_data['steel-grade1']
             ),
             base_material2=BaseMaterial(
-                thickness=float(request.form.get('thickness2')),
-                group='grupa',
-                norm='norma',
-                name=request.form.get('steel-grade2')
+                thickness=float(request.form.get(f'thickness{i}')),
+                group=form_data[f'steel-group{i}'],
+                norm=form_data[f'steel-norm{i}'],
+                name=form_data[f'steel-grade{i}']
             )
         )
+        print(request.form.to_dict())
 
         dwg = generate_svg(message)
         return render_template("wps_input.html", dwg=Markup(dwg
