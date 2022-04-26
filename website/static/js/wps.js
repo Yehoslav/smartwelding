62ed053f015a77d7c2fb5a875f07dc2283cd3d56
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-04-26 17:19:31 +0300

    Created a new view that will create just a preview for the WPS (that is less harsh on the given data)

--- website/static/js/wps.js
+++ website/static/js/wps.js
@@ -3,26 +3,24 @@ simMatCheckbox.checked = true
 simMatCheckbox.addEventListener(
   'change',
   function () {
-    console.log(this);
     if (this.checked) {
-      console.log('I was checked');
       toggleHidden('material2', true)
     } else {
-      console.log('I was unchecked');
       toggleHidden('material2', false)
     }
   }
 )
 
 document
-  .getElementById('filler-material')
+  .getElementById('fillerMaterial')
   .addEventListener(
     'change',
     () => {
-      let data = {
-        electrode: document.getElementById('filler-material').value,
-        norm: document.getElementById('norm-input').value
+      const data = {
+        electrode: document.getElementById('fillerMaterial').value,
+        norm: document.getElementById('normInput').value
       }
+
       fetch('/test', {
         method: 'POST',
         headers: {
@@ -33,14 +31,15 @@ document
         .then(request => request.text())
         .then(
           (text) => {
-            let inpt = document.getElementById('filler-material')
+            const input = document.getElementById('fillerMaterial')
+
             if (text !== 'BAD') {
-              if (inpt.classList.contains('error')) {
-                inpt.classList.remove('error')
+              if (input.classList.contains('error')) {
+                input.classList.remove('error')
               }
-              alert(text);
+              alert(text)
             } else {
-              inpt.classList.add('error')
+              input.classList.add('error')
             }
           }
         )
@@ -48,58 +47,143 @@ document
   )
 
 const standards = [
-  {id: 'eniso2560_a',  name: 'EN ISO 2560-A',  procedure: 111},
-  {id: 'eniso2560_b',  name: 'EN ISO 2560-B',  procedure: 111},
-  {id: 'eniso18275_a', name: 'EN ISO 18275-A', procedure: 111},
-  {id: 'eniso18275_b', name: 'EN ISO 18275-B', procedure: 111},
-  {id: 'eniso3580_a',  name: 'EN ISO 3580-A',  procedure: 111},
-  {id: 'eniso3580_b',  name: 'EN ISO 3580-B',  procedure: 111},
-  {id: 'eniso3581_a',  name: 'EN ISO 3581-A',  procedure: 111},
-  {id: 'eniso3581_b',  name: 'EN ISO 3581-B',  procedure: 111},
-  {id: 'eniso14341_a', name: 'EN ISO 14341-A', procedure: 135},
-  {id: 'eniso14341_a', name: 'EN ISO 14341-A', procedure: 135},
-  {id: 'eniso16834_a', name: 'EN ISO 16834-A', procedure: 135},
-  {id: 'eniso16834_b', name: 'EN ISO 16834-B', procedure: 135},
-  {id: 'eniso21952_a', name: 'EN ISO 21952-A', procedure: 135},
-  {id: 'eniso21952_b', name: 'EN ISO 21952-B', procedure: 135},
-  {id: 'eniso14343_a', name: 'EN ISO 14343-A', procedure: 135},
-  {id: 'eniso14343_b', name: 'EN ISO 14343-B', procedure: 135},
-  {id: 'eniso14341_a', name: 'EN ISO 14341-A', procedure: 131},
-  {id: 'eniso14341_b', name: 'EN ISO 14341-B', procedure: 131},
-  {id: 'eniso16834_a', name: 'EN ISO 16834-A', procedure: 131},
-  {id: 'eniso16834_b', name: 'EN ISO 16834-B', procedure: 131},
-  {id: 'eniso21952_a', name: 'EN ISO 21952-A', procedure: 131},
-  {id: 'eniso21952_b', name: 'EN ISO 21952-B', procedure: 131},
-  {id: 'eniso14343_a', name: 'EN ISO 14343-A', procedure: 131},
-  {id: 'eniso14343_b', name: 'EN ISO 14343-B', procedure: 131},
-  {id: 'eniso636_a',   name: 'EN ISO 636-A',   procedure: 141},
-  {id: 'eniso636_b',   name: 'EN ISO 636-B',   procedure: 141},
-  {id: 'eniso26304_a', name: 'EN ISO 26304-A', procedure: 141},
-  {id: 'eniso26304_b', name: 'EN ISO 26304-B', procedure: 141},
-  {id: 'eniso24598_a', name: 'EN ISO 24598-A', procedure: 141},
-  {id: 'eniso24598_b', name: 'EN ISO 24598-B', procedure: 141},
-  {id: 'eniso14343_a', name: 'EN ISO 14343-A', procedure: 141},
-  {id: 'eniso14343_b', name: 'EN ISO 14343-B', procedure: 141},
+  { id: 'eniso2560_a',  name: 'EN ISO 2560-A',  procedure: 111 },
+  { id: 'eniso2560_b',  name: 'EN ISO 2560-B',  procedure: 111 },
+  { id: 'eniso18275_a', name: 'EN ISO 18275-A', procedure: 111 },
+  { id: 'eniso18275_b', name: 'EN ISO 18275-B', procedure: 111 },
+  { id: 'eniso3580_a',  name: 'EN ISO 3580-A',  procedure: 111 },
+  { id: 'eniso3580_b',  name: 'EN ISO 3580-B',  procedure: 111 },
+  { id: 'eniso3581_a',  name: 'EN ISO 3581-A',  procedure: 111 },
+  { id: 'eniso3581_b',  name: 'EN ISO 3581-B',  procedure: 111 },
+  { id: 'eniso14341_a', name: 'EN ISO 14341-A', procedure: 135 },
+  { id: 'eniso14341_a', name: 'EN ISO 14341-A', procedure: 135 },
+  { id: 'eniso16834_a', name: 'EN ISO 16834-A', procedure: 135 },
+  { id: 'eniso16834_b', name: 'EN ISO 16834-B', procedure: 135 },
+  { id: 'eniso21952_a', name: 'EN ISO 21952-A', procedure: 135 },
+  { id: 'eniso21952_b', name: 'EN ISO 21952-B', procedure: 135 },
+  { id: 'eniso14343_a', name: 'EN ISO 14343-A', procedure: 135 },
+  { id: 'eniso14343_b', name: 'EN ISO 14343-B', procedure: 135 },
+  { id: 'eniso14341_a', name: 'EN ISO 14341-A', procedure: 131 },
+  { id: 'eniso14341_b', name: 'EN ISO 14341-B', procedure: 131 },
+  { id: 'eniso16834_a', name: 'EN ISO 16834-A', procedure: 131 },
+  { id: 'eniso16834_b', name: 'EN ISO 16834-B', procedure: 131 },
+  { id: 'eniso21952_a', name: 'EN ISO 21952-A', procedure: 131 },
+  { id: 'eniso21952_b', name: 'EN ISO 21952-B', procedure: 131 },
+  { id: 'eniso14343_a', name: 'EN ISO 14343-A', procedure: 131 },
+  { id: 'eniso14343_b', name: 'EN ISO 14343-B', procedure: 131 },
+  { id: 'eniso636_a',   name: 'EN ISO 636-A',   procedure: 141 },
+  { id: 'eniso636_b',   name: 'EN ISO 636-B',   procedure: 141 },
+  { id: 'eniso26304_a', name: 'EN ISO 26304-A', procedure: 141 },
+  { id: 'eniso26304_b', name: 'EN ISO 26304-B', procedure: 141 },
+  { id: 'eniso24598_a', name: 'EN ISO 24598-A', procedure: 141 },
+  { id: 'eniso24598_b', name: 'EN ISO 24598-B', procedure: 141 },
+  { id: 'eniso14343_a', name: 'EN ISO 14343-A', procedure: 141 },
+  { id: 'eniso14343_b', name: 'EN ISO 14343-B', procedure: 141 },
 ]
+
+function getWPSData (form) {
+  return {
+    fillerMaterial: form.fillerMaterial.value,
+    fillerNorm: form.fillerNorm.value,
+    fillerThickness: form.fillerThickness.value,
+    process: form.process.value,
+    thickness1: form.thickness1.value,
+    steelGroup1: form.steelGroup1.value,
+    steelNorm1: form.steelNorm1.value,
+    steelGrade1: form.steelGrade1.value,
+    thickness2: form.thickness2.value,
+    steelGroup2: form.steelGroup2.value,
+    steelNorm2: form.steelNorm2.value,
+    steelGrade2: form.steelGrade2.value,
+    similarMaterial: form.similarMaterial.checked,
+    refractaryDiameter: form.refractaryDiameter.value,
+    refractaryType: form.refractaryType.value,
+    position: 'lipsete',
+    gasRootType: form.gasRootType.value,
+    gasCoverType: form.gasCoverType.value,
+    gasRootDebit: form.gasRootDebit.value,
+    gasCoverDebit: form.gasCoverDebit.value,
+    jointType: 'lipseste',
+    preheat: form.preheat.value,
+    postheat: form.postheat.value
+  }
+}
+
+// Is the given number acceptable
+function checkNumberField (inputField) {
+  console.log(`Checking: ${inputField.id};\nWhich has the value of ${inputField.value}`)
+  const thickness = parseFloat(inputField.value)
+  if (thickness > 0) {
+    inputField.classList.remove('error')
+    return 0
+  } else {
+    inputField.classList.add('error')
+    return 1
+  }
+}
+
+async function submitWPS (form) {
+  const data = getWPSData(form)
+  console.log('Check if fields are filled')
+  let error = 0
+  for (const field in data) { if (data[field] === '') error++ }
+  console.log(`number of err fields ${error}`)
+
+  if (error === 0) {
+    fetch('/wps', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(respons => respons.text())
+      .then(text => { document.getElementById('wps-view').innerHTML = text })
+  } else {
+    fetch('/wps/preview', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(respons => respons.text())
+      .then(text => { document.getElementById('wps-view').innerHTML = text })
+  }
+}
 
 changeFillerStandards()
 
 class CodeError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = this.constructor.name;
+  constructor (message) {
+    super(message)
+    this.name = this.constructor.name
   }
 }
 
-function toggleHidden(elementId, toHide) {
-  let el = document.getElementById(elementId)
+function toggleHidden (elementId, toHide) {
+  const element = document.getElementById(elementId)
   if (toHide === true) {
-    el.classList.add('hidden')
+    element.classList.add('hidden')
   } else {
-    if (el.classList.contains('hidden')) {
-      el.classList.remove('hidden')
+    if (element.classList.contains('hidden')) {
+      element.classList.remove('hidden')
     }
   }
+}
+
+/**
+ * Get a list of filler material diameter.
+ * @summary The function will send a post request to the python side and recieve
+ * a list of dimensions for the filler material of the given welding process.
+ * @param {string} wProcess: The code of the welding process as given in TODO ISO standard
+ * @return {[number]}: Returns an array with the diameters.
+ */
+async function getElDiameters (wProcess) {
+  const data = await fetch(`/data/std/iso544?process=${wProcess}&field=diameters`)
+    .then(request => request.json())
+    .then(data => data)
+  return data
 }
 
 /*
@@ -107,10 +191,16 @@ function toggleHidden(elementId, toHide) {
  * material
  * TODO: Change the name of the function (changeFillerStandard should be parte of the function)
  */
-function changeFillerStandards () {
-  const procedeu = document.getElementById('procedee').value
+async function changeFillerStandards () {
+  const procedeu = document.getElementById('process').value
 
   // Wich fields should be hidden based on the welding procedure
+  // TODO: get the hidden fields from the electrode type, not the used process
+  //       Ex: 111: consumable rod
+  //           131, 135: consumable wire
+  //           141: refractary
+  // TODO: Each welding process should have a field based on wich the toggleHidden will be called
+  //       Ex: gas: true; refractary: false
   const toHide = ((proc) => {
     switch (proc) {
       case '111':
@@ -121,7 +211,7 @@ function changeFillerStandards () {
       case '141':
         return [false, false]
       default:
-        alert("Procedeu necunoscut.")
+        alert('Procedeu necunoscut.')
     }
   })(procedeu)
 
@@ -130,54 +220,37 @@ function changeFillerStandards () {
 
   // TODO: The standard list should be included in a separate file. (to avoid
   // errors as on row 10)
-  const stdList = standards.filter((value) => value.procedure == procedeu)
-                           .map((standard) => standard.name)
+  const stdList = standards
+    .filter((value) => value.procedure === parseInt(procedeu))
+    .map((standard) => standard.name)
   const optList = document.getElementById('norm-list')
-  console.log(stdList);
+
   // Hide the previous standards
-  const docF = document.createDocumentFragment();
-  for (i=optList.children.length; i>0; i--) {
-      docF.appendChild(optList.children[i-1])
+  const docF = document.createDocumentFragment()
+  for (let i = optList.children.length; i > 0; i--) {
+    docF.appendChild(optList.children[i - 1])
   }
 
   // Append the new standards
-  for (std of stdList) {
-    let opt = document.createElement('option')
+  for (const std of stdList) {
+    const opt = document.createElement('option')
     opt.value = std
     optList.appendChild(opt)
   }
 
-  const diams = {
-    '111': [2, 3.5, 4, 5],
-    '131': [0.6, 0.8, 1.2, 1.6],
-    '135': [0.6, 0.8, 1.2, 1.6],
-    '141': [0.6, 0.8, 1.2, 1.6],
-  }
-  const diamList = document.getElementById('filler_thickness')
+  const diamList = document.getElementById('fillerThickness')
+  const diams = await getElDiameters(procedeu)
 
-  console.log(diams);
   // Hide the previous standards
-  for (i=diamList.children.length; i>0; i--) {
-      docF.appendChild(diamList.children[i-1])
+  for (let i = diamList.children.length; i > 0; i--) {
+    docF.appendChild(diamList.children[i - 1])
   }
 
   // Append the new standards
-  for (diam of diams[procedeu]) {
-    let opt = document.createElement('option')
-    opt.value = diam
-    opt.innerHTML = diam
-    diamList.appendChild(opt)
+  for (const diam of diams) {
+    const option = document.createElement('option')
+    option.value = diam
+    option.innerHTML = diam
+    diamList.appendChild(option)
   }
-
-}
-
-// Is the given number acceptable
-function checkNumberField(inputField) {
-  var thickness = inputField.value;
-  if (parseFloat(thickness) <= 0) {
-    inputField.classList.add("error");
-  } else {
-    inputField.classList.remove("error");
-  }
-  // decode('E4201NiA12H5', patternA, valueListA)
 }
--- website/wps_view.py
+++ website/wps_view.py
@@ -2,6 +2,7 @@
 from flask import Blueprint
 from flask import render_template, request, jsonify
 from flask import Markup
+from website.welding.svg_edit import WPSData, generate_svg, FillerMaterial, BaseMaterial, Refractary, ProjectData, Gas, HeatTreatment
 
 wps_view = Blueprint('wps_view', __name__)
 
@@ -21,6 +22,7 @@ def wpss():
     if request.method == 'POST':
         from website.welding.standards import ISO2560, SubType, ISOError
         data = request.get_json()
+
         try:
             match data:
                 case {'electrode': el, 'norm': 'EN ISO 2560-A'}:
@@ -29,9 +31,8 @@ def wpss():
                     val = ISO2560().decode(el, SubType.B)
                 case _:
                     val = data
-
-            print(val)
             return val, 200
+
         except Exception as err:
             print(err)
             return 'BAD', 200
@@ -41,41 +42,90 @@ def wpss():
 
 
 @wps_view.route('/wps', methods=['GET', 'POST'])
-def edit_wps():
-    """The homepage."""
+def generate_wps():
+    """Generate and return WPS."""
     if request.method == 'POST':
         # TODO: Add to chash template.svg, the wps_data, and the generated svg
-        from website.welding.svg_edit import WPSData
-        from website.welding.svg_edit import generate_svg
-        from website.welding.svg_edit import FillerMaterial
-        from website.welding.svg_edit import BaseMaterial
-        form_data = request.form.to_dict()
-        i = 1 if 'similarMaterial' in form_data else 2
+        form_data = request.get_json()
+        i = 1 if form_data['similarMaterial'] else 2
         message = WPSData(
             filler_material=FillerMaterial(
-                name=form_data['filler-material'],
-                norm=form_data['filler-norm'],
-                thickness=float(form_data['filler_thickness']),
+                name=form_data['fillerMaterial'],
+                norm=form_data['fillerNorm'],
+                thickness=float(form_data['fillerThickness']),
                 dry='dry' in form_data,
-                procedure_type=form_data['procedee'],
+                procedure_type=form_data['process'],
             ),
             base_material1=BaseMaterial(
                 thickness=float(form_data['thickness1']),
-                group=form_data['steel-group1'],
-                norm=form_data['steel-norm1'],
-                name=form_data['steel-grade1']
+                group=form_data['steelGroup1'],
+                norm=form_data['steelNorm1'],
+                name=form_data['steelGrade1']
             ),
             base_material2=BaseMaterial(
-                thickness=float(request.form.get(f'thickness{i}')),
-                group=form_data[f'steel-group{i}'],
-                norm=form_data[f'steel-norm{i}'],
-                name=form_data[f'steel-grade{i}']
+                thickness=float(form_data[f'thickness{i}']),
+                group=form_data[f'steelGroup{i}'],
+                norm=form_data[f'steelNorm{i}'],
+                name=form_data[f'steelGrade{i}']
             )
         )
-        print(request.form.to_dict())
 
         dwg = generate_svg(message)
-        return render_template("wps_input.html", dwg=Markup(dwg
-                                                            .replace("210mm", "157.5mm")
-                                                            .replace("297mm", "222.75mm")))
+        return dwg.replace("210mm", "157.5mm").replace("297mm", "222.75mm")
+
+    return render_template("wps_input.html", dwg="")
+
+
+@wps_view.route('/wps/preview', methods=['GET', 'POST'])
+def preview_wps():
+    """Generate and return WPS."""
+    if request.method == 'POST':
+        # TODO: Add to chash template.svg, the wps_data, and the generated svg
+        form_data = request.get_json()
+        i = 1 if form_data['similarMaterial'] else 2
+        message = WPSData(
+            filler_material=FillerMaterial(
+                name=form_data['fillerMaterial'],
+                norm=form_data['fillerNorm'],
+                thickness=float(form_data['fillerThickness']) if form_data['fillerThickness'] else '',
+                dry='dry' in form_data,
+            ),
+            base_material1=BaseMaterial(
+                thickness=float(form_data['thickness1']) if form_data['thickness1'] else '',
+                group=form_data['steelGroup1'],
+                norm=form_data['steelNorm1'],
+                name=form_data['steelGrade1']
+            ),
+            base_material2=BaseMaterial(
+                thickness=float(form_data[f'thickness{i}']) if form_data[f'thickness{i}'] else '',
+                group=form_data[f'steelGroup{i}'],
+                norm=form_data[f'steelNorm{i}'],
+                name=form_data[f'steelGrade{i}']
+            ),
+            refractary=Refractary(
+                diameter=form_data['refractaryDiameter'],
+                electrode_type=form_data['refractaryType']
+            ),
+            gas_root=Gas(
+                debit=form_data['gasRootDebit'],
+                gas_type=form_data['gasRootType']
+            ),
+            gas_cover=Gas(
+                debit=form_data['gasCoverDebit'],
+                gas_type=form_data['gasCoverType']
+            ),
+            project_data=ProjectData(
+                position=form_data['position'],
+                w_process=form_data['process'],
+                joint_type=form_data['jointType']
+            ),
+            heat_tratment=HeatTreatment(
+                preheat=form_data['preheat'],
+                postheat=form_data['postheat']
+            )
+        )
+
+        dwg = generate_svg(message)
+        return dwg.replace("210mm", "157.5mm").replace("297mm", "222.75mm")
+
     return render_template("wps_input.html", dwg="")
