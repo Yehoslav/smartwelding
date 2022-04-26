306ed18e0b57f2e204cff33bfac45051f93ea9af
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-04-26 17:13:30 +0300

    A view used to do the calculations.

--- website/param_view.py
+++ website/param_view.py
@@ -11,6 +11,16 @@ def parameters():
     return 'Aici se calculeaza parametrii de sudare'
 
 
+@param_view.route('/carb-eq', methods=['GET', 'POST'])
+def carb_eq():
+    if request.method == 'POST':
+        from website.welding.carbon_echivalent import Steel, ceiiw
+        data: dict = request.get_json()
+        float_data = {key: float(value) for key, value in data.items()}
+        return "ciiw: "+str(ceiiw(Steel(float_data)))
+    return 'Calculul carbonului echivalent nu a fost implementat inca'
+
+
 @param_view.route('/migmag')
 def migmag():
     return render_template('migmag.html')
