a71ddd44f9b685c5f48c64ffb897a0aba5a738ed
Yehoslav Rudenco <yehoslavrudenco@gmail.com>
2022-04-26 17:16:51 +0300

    Added the data blueprint

--- website/__init__.py
+++ website/__init__.py
@@ -9,8 +9,10 @@ def create_app():
 
     from .wps_view import wps_view
     from .param_view import param_view
+    from .data_view import data_view
 
     app.register_blueprint(wps_view, url_prefix='/')
     app.register_blueprint(param_view, url_prefix='/params')
+    app.register_blueprint(data_view, url_prefix='/data')
 
     return app
